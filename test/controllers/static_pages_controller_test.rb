fe28dd0b7c2e3ee65822945c3cf12a22bb14bbde
tsunozemi <nyanta3324@gmail.com>
2019-10-07 11:59:33 +0900

    DRY html layout

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,6 +1,11 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  
+  def setup
+      @base_tittle = "Ruby on Rails Tutorial Sample App"
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
